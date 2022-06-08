520555bb6bb1b9dbf89053b855c36386eb2300a6
bitasd <bitasd1m.bs@gmail.com>
2022-06-07 20:50:44 -0400

    fixed errors and added the shortest path

--- build_nx.py
+++ build_nx.py
@@ -8,66 +8,86 @@ def gdf_to_nx(gdf_network):
     net = nx.MultiDiGraph()
     net.graph['crs'] = gdf_network.crs
     # fields = list(gdf_network.columns)
-    fields = ['ID_TRC_int', 'CLASSE', 'SENS_CIR', 'slope', 'slope_edit', 'length', 'lts', 'lts_negD', 'lts_c', 'umbrell_id', 'geometry', 'signed_sl', 'fid'] #
+    fields = ['ID_TRC_int', 'CLASSE', 'SENS_CIR', 'slope', 'slope_edit', 'length', 'lts', 'lts_negD', 'lts_c', 'umbrell_id', 'geometry', 'signed_sl', 'fid', 'TYPE_VOIE'] #
 
     for index, row in gdf_network.iterrows():
+        try:
+            first = row.geometry.coords[0]
+            last = row.geometry.coords[-1]
 
-        first = row.geometry.coords[0]
-        last = row.geometry.coords[-1]
+        except NotImplementedError:
+            first = list(row.geometry.geoms)[0].coords[0]
+            last = list(row.geometry.geoms)[-1].coords[0]
+            print("index for MultiLinestring", index)
+
 
         data = [row[f] for f in fields]
         attributes = dict(zip(fields, data))
         attr_rev = attributes.copy()
 
-        if attributes['SENS_CIR'] == -1:
-            # add a REVERSED edge w with-flow lane
-
-
-            attr_rev['slope_edit'] = attributes['slope_edit'] * (-1)
-            attr_rev['signed_sl'] = attributes['signed_sl'] * (-1)
-            attr_rev['lts_final'] = attributes['lts']
-            net.add_edge(last, first, **attr_rev)
-
-            if attributes['lts_c'] != -9999:
-                # add an edge w contraflow lane
-                attributes['lts_final'] = attributes['lts_c']
-                net.add_edge(first, last, **attributes)
-
-        elif attributes['SENS_CIR'] == 1:
-            attributes['lts_final'] = attributes['lts']
-            net.add_edge(first, last, **attributes)
-
-            if attributes['lts_c'] != -9999:
-                # add a REVERSED edge with contraflow lane
-                attr_rev['slope_edit'] = attributes['slope_edit'] * (-1)
-                attr_rev['signed_sl'] = attributes['signed_sl'] * (-1)
-                attr_rev['lts_final'] = attributes['lts_c']
-                net.add_edge(last, first, **attr_rev)
-
-        elif attributes['SENS_CIR'] == 0:
-            # add edge
+        if attributes['TYPE_VOIE'] in [5,6,7]:  # multi-purpose trail, sidewalk-level path, path outside way
+            # add path
             attributes['lts_final'] = attributes['lts']
             net.add_edge(first, last, **attributes)
 
             # add reversed edge
             attr_rev['slope_edit'] = attributes['slope_edit'] * (-1)
             attr_rev['signed_sl'] = attributes['signed_sl'] * (-1)
-
-            if attributes['lts_negD']!= -9999:  # asymmetric situation
-                attr_rev['lts_final'] = attributes['lts_negD']
-
-            else:  # symmetric situation
-                attr_rev['lts_final'] = attributes['lts']
-
+            attr_rev['lts_final'] = attributes['lts']
             net.add_edge(last, first, **attr_rev)
 
-        else:
-            print('elsed out')
+        else: #
+
+            if attributes['SENS_CIR'] == -1:
+                # add a REVERSED edge w with-flow lane
+                attr_rev['slope_edit'] = attributes['slope_edit'] * (-1)
+                attr_rev['signed_sl'] = attributes['signed_sl'] * (-1)
+                attr_rev['lts_final'] = attributes['lts']
+                net.add_edge(last, first, **attr_rev)
+
+                if attributes['lts_c'] != -9999:
+                    # add an edge w contraflow lane
+                    attributes['lts_final'] = attributes['lts_c']
+                    net.add_edge(first, last, **attributes)
+
+            elif attributes['SENS_CIR'] == 1:
+                attributes['lts_final'] = attributes['lts']
+                net.add_edge(first, last, **attributes)
+
+                if attributes['lts_c'] != -9999:
+                    # add a REVERSED edge with contraflow lane
+                    attr_rev['slope_edit'] = attributes['slope_edit'] * (-1)
+                    attr_rev['signed_sl'] = attributes['signed_sl'] * (-1)
+                    attr_rev['lts_final'] = attributes['lts_c']
+                    net.add_edge(last, first, **attr_rev)
+
+            elif attributes['SENS_CIR'] == 0:
+                # add edge
+                attributes['lts_final'] = attributes['lts']
+                net.add_edge(first, last, **attributes)
+
+                # add reversed edge
+                attr_rev['slope_edit'] = attributes['slope_edit'] * (-1)
+                attr_rev['signed_sl'] = attributes['signed_sl'] * (-1)
+
+                if attributes['lts_negD']!= -9999:  # asymmetric situation
+                    attr_rev['lts_final'] = attributes['lts_negD']
+
+                else:  # symmetric situation
+                    attr_rev['lts_final'] = attributes['lts']
+
+                net.add_edge(last, first, **attr_rev)
+
+            else:
+                print('elsed out')
+                attr_rev['lts_final'] = attributes['lts']
+
+                net.add_edge(last, first, **attr_rev)
 
     return net
 
 
-def nx_to_gdf(net, nodes=True, edges=True):
+def nx_to_gdf(net, nodes=True, edges=True, crs=2950):
     # generate nodes and edges geodataframes from graph
     if nodes is True:
         print('nodes')
@@ -75,14 +95,16 @@ def nx_to_gdf(net, nodes=True, edges=True):
 
         gdf_nodes = geopandas.GeoDataFrame(list(node_data), geometry=[Point(i, j) for i, j in node_xy])
         print("gdf_nodes: ", len(gdf_nodes))
-        gdf_nodes.crs = net.graph['crs']
+        # gdf_nodes.crs = net.graph['crs']
+        gdf_nodes.crs = crs
 
     if edges is True:
         print('edges')
         starts, ends, edge_data = zip(*net.edges(data=True))
         gdf_edges = geopandas.GeoDataFrame(list(edge_data))
         print("gdf_edges: ", len(gdf_edges))
-        gdf_edges.crs = net.graph['crs']
+        # gdf_edges.crs = net.graph['crs']
+        gdf_nodes.crs = crs
 
     if nodes is True and edges is True:
         print('1')
